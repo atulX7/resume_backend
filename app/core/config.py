bc67ad5fd498d58c72bd864f8200e13f1ee9c713
Atul Yadav <148873671+atulX7@users.noreply.github.com>
2025-03-10 21:15:50 +0530

    Add files via upload

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings:
     PROJECT_NAME: str = "Resume Builder API"
     SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")
     DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+    ALLOW_ORIGINS: list = os.getenv("ALLOW_ORIGINS", "http://localhost:3000").split(",")
 
     # AWS Credentials (Ensure these are set in .env)
     AWS_ACCESS_KEY: str = os.getenv("AWS_ACCESS_KEY")
