9e8a7e566a6a3f612cd051ae9b0639eedef5875f
neelam <neelamyadav1@kpmg.com>
2025-04-02 10:32:01 +0530

    test mock

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,4 +1,4 @@
-MAX_QUESTIONS_PER_SESSION = 9
+MAX_QUESTIONS_PER_SESSION = 2
 INTRO_QUESTION = "Hello and welcome! It's great to have you here today. I'm Alex, and I'm looking forward to our conversation. To start things off, could you please introduce yourself and share a bit about your career journey? Feel free to include any experiences or achievements that you believe would be important for us to know as we begin the interview."
 GOOGLE_USER_INFO_URL = "https://www.googleapis.com/oauth2/v3/userinfo"
 GOOGLE_TOKEN_INFO_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo"
